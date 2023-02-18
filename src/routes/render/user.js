bcf4b16da976b92b88a65836211ff504316a9088
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2023-02-17 21:13:08 -0500

    Rotate job ids on job

--- src/routes/render/user.js
+++ src/routes/render/user.js
@@ -1,39 +1,39 @@
-const express = require("express");
-const app = express.Router();
+const express = require("express")
+const app = express.Router()
 
-const RenderJob = require("../../lib/classes/RenderJob.js");
+const RenderJob = require("../../lib/classes/RenderJob.js")
 
 app.get("/:id", async (request, response) => {
-	const { params, query } = request;
-	const job = new RenderJob();
-	let body = {};
+	const { params, query } = request
+	const job = new RenderJob()
+	let body = {}
 
-	const headshot = await job.RenderHeadshot(params.id).catch((_) => _);
+	const headshot = await job.RenderHeadshot(params.id).catch((_) => _)
 	if (headshot?.message) {
-		job.Stop();
-		return response.status(500).json({ error: headshot.message });
+		job.Stop()
+		return response.status(500).json({ error: headshot.message })
 	}
-	body.headshot = headshot;
+	body.headshot = headshot
 
-	const bodyshot = await job.RenderBodyshot(params.id).catch((_) => _);
+	const bodyshot = await job.RenderBodyshot(params.id).catch((_) => _)
 	if (bodyshot?.message) {
-		job.Stop();
-		return response.status(500).json({ error: bodyshot.message });
+		job.Stop()
+		return response.status(500).json({ error: bodyshot.message })
 	}
-	body.bodyshot = bodyshot;
+	body.bodyshot = bodyshot
 
 	if (query.three_d) {
-		const three_d = await job.RenderBodyshot(params.id, true).catch((_) => _);
+		const three_d = await job.RenderBodyshot(params.id, true).catch((_) => _)
 		if (three_d?.message) {
-			job.Stop();
-			return response.status(500).json({ error: three_d.message });
+			job.Stop()
+			return response.status(500).json({ error: three_d.message })
 		}
-		body.three_d = three_d;
+		body.three_d = three_d
 	}
 
-	job.Stop();
+	job.Stop()
 
-	return response.json(body);
-});
+	return response.json(body)
+})
 
-module.exports = app;
+module.exports = app
