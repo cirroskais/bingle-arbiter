0784446a61ab3205221c5cad69ebb54e068dc8ef
I-Have-An-Issue <34550332+I-Have-An-Issue@users.noreply.github.com>
2023-02-20 04:12:51 -0500

    Document, better endpoints

--- src/routes/render/game.js
+++ src/routes/render/game.js
@@ -3,12 +3,16 @@ const app = express.Router()
 
 const RenderJob = require("../../lib/classes/RenderJob.js")
 
-app.get("/:id", async (request, response) => {
-	const job = new RenderJob()
-	const result = await job.RenderPlace(request.params.id, process.env.RENDER_BASE64).catch((_) => _)
+// app.get("/:id", async (request, response) => {
+// 	const job = new RenderJob()
+// 	const result = await job.RenderPlace(request.params.id, process.env.RENDER_BASE64).catch((_) => _)
 
-	if (result?.message) return response.status(500).json({ error: result.message })
-	else return response.end(result)
+// 	if (result?.message) return response.status(500).json({ error: result.message })
+// 	else return response.end(result)
+// })
+
+app.get("*", (request, response) => {
+	response.status(501).end("Not Implemented")
 })
 
 module.exports = app
